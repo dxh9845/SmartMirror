3580ce07a66e01efbec4e728085bd8b579d05f8d
Daniel Herzig <dxh9845@rit.edu>
2018-07-27 21:51:12 -0400

    Added datetime logic to Todoist tasks. Improved legibility.

--- src/components/Todoist/TodoistProject.vue
+++ src/components/Todoist/TodoistProject.vue
@@ -1,13 +1,21 @@
 <template>
-    <b-container>
-        <h4 class="p-2">Todoist</h4>
+    <b-container fluid>
+        <h2 class="p-2 m-0">Todoist</h2>
         <b-row>
-            <b-col sm="6"
+            <b-col md="6"
                     v-for="proj in projects" 
                     v-bind:data="proj" 
                     v-bind:key="proj.id">
-                <b-alert class="proj" show variant="dark" >
-                    {{ proj.name }}
+                <b-alert class="proj" 
+                    show variant="dark" >
+                    <h3>{{ proj.name }}</h3>
+                    <keep-alive>
+                    <todoist-task v-for="task in proj.tasks"
+                        v-bind:key="task.id"
+                        v-bind:taskTitle="task.content"
+                        v-bind:taskDueDate="task.due">
+                    </todoist-task>
+                    </keep-alive>
                 </b-alert>
             </b-col>
         </b-row>
@@ -15,27 +23,49 @@
 </template>
 
 <script>
-    
+    import TodoistTask from './TodoistTask';
+
     export default {
         name: 'TodoistProject',
+        components: {
+            TodoistTask
+        },
         methods: {
+            filterProjects(project) {
+                return ["Personal", "Inbox", "Work", "School"].includes(project.name);
+            },
         },
         data() {
             return {
                 projects: null
             }
         },
-        created() {
 
-            fetch(process.env.TODOIST_API_ENDPOINT, {
-                headers: {
-                    "Authorization": `Bearer ${process.env.TODOIST_API_TOKEN}`
-                }
+        created() {
+            let headersObj = {
+                "Authorization": `Bearer ${process.env.TODOIST_API_TOKEN}`
+            }
+            fetch(process.env.TODOIST_API_ENDPOINT + '/projects', {
+                headers: headersObj
             })
-            .then( res => { return res.json(); })
+            .then( res => { return res.json() })
             .then( jsonRes => {
                 console.log(jsonRes)
-                this.projects = jsonRes;
+                this.projects = jsonRes.filter(this.filterProjects);
+
+                // Now call todoist for tasks
+                return fetch(process.env.TODOIST_API_ENDPOINT + '/tasks', {
+                    headers: headersObj
+                })
+            })
+            .then( res => { return res.json() })
+            .then( jsonRes => {
+                console.log(jsonRes);
+                this.projects.forEach(project => {
+                    let projTasks = jsonRes.filter( task => { return project.id == task.project_id })
+                    project.tasks = projTasks;
+                });
+                this.projects = this.projects.filter(p => { return p.tasks.length != 0});
             })
             .catch( err => console.error(err))
         }
@@ -44,11 +74,20 @@
 
 
 <style lang="scss" scoped>
-    h4 {
+    h2 {
         font-weight: 100;
+        letter-spacing: 2px;
+        font-family: 'OstrichSansHeavy', 'Arial Narrow', Arial, sans-serif;
         text-align: center;
         color: #FFF;
     }
+
+    h3 {
+        font-weight: 200;
+        font-family: "OstrichSansHeavy";
+        letter-spacing: 2px;
+    }
+
     .proj {
         color: #FFF;
         background-color: transparent;
--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import App from './App.vue'
 // import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
+Vue.config.performance = true;
+
 Vue.use(BootstrapVue)
+Vue.use(require('vue-moment'))
 
 new Vue({
   el: '#app',
-  template: '<App/>',
+  template: '<App/>', 
   components: { App }
 })
